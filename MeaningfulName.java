41c2fdff9b0101707d19b48ac1aec9a613246f69
kdtTetz <xenosign@naver.com>
2025-07-14 20:56:01 +0900

    Update MeaningfulName.java

--- MeaningfulName.java
+++ MeaningfulName.java
@@ -1,4 +1,6 @@
+import javax.xml.crypto.Data;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class MeaningfulName {
@@ -64,5 +66,17 @@ public class MeaningfulName {
         }
 
         return flaggedCells;
+    }
+
+    class DtaRcrd102 {
+        private Date genymdhms;
+        private Date modymdhms;
+        private final String pszqint = "102";
+    }
+
+    class Customer {
+        private Date generationTimestamp;
+        private Date modificationTimestamp;
+        private final String recordId = "102";
     }
 }
